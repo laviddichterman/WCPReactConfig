97303b5dac0d57cc795800ae930099a9513487d8
Dave Lichterman <dave@windycitypie.com>
2020-01-25 00:59:14 -0800

    convert app to functional component

--- src/App.js
+++ src/App.js
@@ -1,4 +1,4 @@
-import React, { Component } from "react";
+import React, { useState, useEffect } from "react";
 import "bootstrap/dist/css/bootstrap.min.css";
 import socketIOClient from "socket.io-client";
 import BlockOffComp from "./components/blockoff.component";
@@ -8,71 +8,61 @@ import SettingsComp from "./components/settings.component";
 const WDateUtils = require("@wcp/wcpshared");
 
 const store = "Windy City Pie";
-const ENDPOINT = "http://127.0.0.1:4001";
+const ENDPOINT = "http://localhost:4001";
 
-class App extends Component {
-  constructor() {
-    super();
-    this.state = {
-      socket: socketIOClient(ENDPOINT),
-      SERVICES: ["Pick-up", "Dine-In", "Delivery"],
-      BLOCKED_OFF: [],
-      LEADTIME: [40, 40, 1440],
-      SETTINGS: {
-        additional_pizza_lead_time: 5, //to deprecate
-        time_step: 15,
-        pipeline_info: {
-          baking_pipeline: [{slots:3, time:3}, {slots: 12, time:11}, {slots:10, time:12.5}],
-          transfer_padding: .5},
-        operating_hours: [
-          [[], [], [], [], [], [], []],[[], [], [], [], [], [], []],[[], [], [], [], [], [], []]
-        ]
-      },
-      can_submit: true
-    };
-  }
-  updateSettings = (data) => {
-    this.setState({SETTINGS: data});
-  }
-  componentDidMount = () => {
-    this.state.socket.on("WCP_SERVICES", data => this.setState({ SERVICES: data}));
-    this.state.socket.on("WCP_BLOCKED_OFF", data => this.setState({ BLOCKED_OFF: data}));
-    this.state.socket.on("WCP_LEAD_TIMES", data => this.setState({ LEADTIME: data}));
-    this.state.socket.on("WCP_SETTINGS", data => (this.updateSettings(data)));
+const App = () => {
+  const [ socket, setSocket ] = useState(socketIOClient(ENDPOINT));
+  const [ SERVICES, setSERVICES ] = useState(["Pick-up", "Dine-In", "Delivery"]);
+  const [ BLOCKED_OFF, setBLOCKED_OFF ] = useState([]);
+  const [ LEADTIME, setLEADTIME ] = useState([40, 40, 1440]);
+  const [ SETTINGS, setSETTINGS ] = useState({
+    additional_pizza_lead_time: 5, //to deprecate
+    time_step: 15,
+    pipeline_info: {
+      baking_pipeline: [{slots:3, time:3}, {slots: 12, time:11}, {slots:10, time:12.5}],
+      transfer_padding: .5},
+    operating_hours: [
+      [[], [], [], [], [], [], []],[[], [], [], [], [], [], []],[[], [], [], [], [], [], []]
+    ]
+  });
+  const [ canSubmit, setCanSubmit ] = useState(true);
 
-  }
+  useEffect(() => {
+    socket.on("WCP_SERVICES", data => setSERVICES(data));
+    socket.on("WCP_BLOCKED_OFF", data => setBLOCKED_OFF(data));
+    socket.on("WCP_LEAD_TIMES", data => setLEADTIME(data));
+    socket.on("WCP_SETTINGS", data => setSETTINGS(data));
+  });
 
-  onSubmit = () => {
-    this.state.socket.emit("WCP_SERVICES", this.state.SERVICES);
-    this.state.socket.emit("WCP_BLOCKED_OFF", this.state.BLOCKED_OFF);
-    this.state.socket.emit("WCP_SETTINGS", this.state.SETTINGS);
-    this.state.socket.emit("WCP_LEAD_TIMES", this.state.LEADTIME);
+  const onSubmit = () => {
+    socket.emit("WCP_SERVICES", SERVICES);
+    socket.emit("WCP_BLOCKED_OFF", BLOCKED_OFF);
+    socket.emit("WCP_SETTINGS", SETTINGS);
+    socket.emit("WCP_LEAD_TIMES", LEADTIME);
   }
 
 
-  onChangeLeadTimes = (i, e) => {
-    const leadtimes = this.state.LEADTIME.slice();
+  const onChangeLeadTimes = (i, e) => {
+    const leadtimes = LEADTIME.slice();
     leadtimes[i] = e;
-    this.setState({
-      LEADTIME: leadtimes
-    });
+    setLEADTIME(leadtimes);
   }
 
-  onChangeAdditionalPizzaLeadTime = (e) => {
-    const new_settings = JSON.parse(JSON.stringify(this.state.SETTINGS));
+  const onChangeAdditionalPizzaLeadTime = (e) => {
+    const new_settings = JSON.parse(JSON.stringify(SETTINGS));
     new_settings.additional_pizza_lead_time = e;
-    this.setState({SETTINGS:new_settings});
+    setSETTINGS(new_settings);
   }
 
-  onChangeTimeStep = (e) => {
-    const new_settings = JSON.parse(JSON.stringify(this.state.SETTINGS));
+  const onChangeTimeStep = (e) => {
+    const new_settings = JSON.parse(JSON.stringify(SETTINGS));
     new_settings.time_step = parseInt(e);
-    this.setState({SETTINGS:new_settings});
+    setSETTINGS(new_settings);
   }
 
 
-  addBlockedOffInterval = (parsed_date, interval, service_selection) => {
-    const new_blocked_off = this.state.BLOCKED_OFF.slice();
+  const addBlockedOffInterval = (parsed_date, interval, service_selection) => {
+    const new_blocked_off = BLOCKED_OFF.slice();
     // iterate over services
     for (var service_index in service_selection) {
       if (service_selection[service_index]) {
@@ -80,75 +70,72 @@ class App extends Component {
         WDateUtils.AddIntervalToService(service_index, parsed_date, interval, new_blocked_off);
       }
     }
-    this.setState({BLOCKED_OFF: new_blocked_off});
+    setBLOCKED_OFF(new_blocked_off);
   }
 
-  removeBlockedOffInterval = (service_index, day_index, interval_index) => {
+  const removeBlockedOffInterval = (service_index, day_index, interval_index) => {
     const new_blocked_off = WDateUtils.RemoveInterval(
       service_index,
       day_index,
       interval_index,
-      this.state.BLOCKED_OFF);
-    this.setState({BLOCKED_OFF: new_blocked_off});
+      BLOCKED_OFF);
+      setBLOCKED_OFF(new_blocked_off);
   }
 
-  addOperatingHours = (service_index, day_index, interval) => {
-    const new_settings = JSON.parse(JSON.stringify(this.state.SETTINGS));
+  const addOperatingHours = (service_index, day_index, interval) => {
+    const new_settings = JSON.parse(JSON.stringify(SETTINGS));
     WDateUtils.AddIntervalToOperatingHours(
       service_index,
       day_index,
       interval,
       new_settings.operating_hours);
-    this.setState({SETTINGS: new_settings});
+    setSETTINGS(new_settings);
   }
 
-  removeOperatingHours = (service_index, day_index, interval_index) => {
-    const new_settings = JSON.parse(JSON.stringify(this.state.SETTINGS));
+  const removeOperatingHours = (service_index, day_index, interval_index) => {
+    const new_settings = JSON.parse(JSON.stringify(SETTINGS));
     const new_operating_hours = WDateUtils.RemoveIntervalFromOperatingHours(
       service_index,
       day_index,
       interval_index,
       new_settings.operating_hours);
     new_settings.operating_hours = new_operating_hours;
-    this.setState({SETTINGS: new_settings});
+    setSETTINGS(new_settings);
   }
-
-  render() {
-    return (
-        <div className="ordercfg container-fluid">
-          <h2>{store} Order Configuration</h2>
-          <nav className="navbar navbar-expand-lg navbar-light bg-light">
-            <div className="collpase navbar-collapse">
-            </div>
-          </nav>
-          <br/>
-            <div className="row no-gutters">
-              <BlockOffComp
-                SERVICES={this.state.SERVICES}
-                blocked_off={this.state.BLOCKED_OFF}
-                onSubmit={this.addBlockedOffInterval}
-                RemoveInterval={this.removeBlockedOffInterval}
-                SETTINGS={this.state.SETTINGS}
-              />
-            </div>
-            <LeadTimesComp
-              leadtimes={this.state.LEADTIME}
-              SERVICES={this.state.SERVICES}
-              onChange={this.onChangeLeadTimes}/>
-            <SettingsComp
-              SERVICES={this.state.SERVICES}
-              settings={this.state.SETTINGS}
-              onChangeTimeStep={this.onChangeTimeStep}
-              onChangeAdditionalPizzaLeadTime={this.onChangeAdditionalPizzaLeadTime}
-              onAddOperatingHours={this.addOperatingHours}
-              onRemoveOperatingHours={this.removeOperatingHours}
-            />
-            <button className="btn btn-dark" onClick={this.onSubmit} disabled={!this.state.can_submit}>Update Settings</button>
+  return (
+      <div className="ordercfg container-fluid">
+        <h2>{store} Order Configuration</h2>
+        <nav className="navbar navbar-expand-lg navbar-light bg-light">
+          <div className="collpase navbar-collapse">
           </div>
+        </nav>
+        <br/>
+          <div className="row no-gutters">
+            <BlockOffComp
+              SERVICES={SERVICES}
+              blocked_off={BLOCKED_OFF}
+              onSubmit={addBlockedOffInterval}
+              RemoveInterval={removeBlockedOffInterval}
+              SETTINGS={SETTINGS}
+            />
+          </div>
+          <LeadTimesComp
+            leadtimes={LEADTIME}
+            SERVICES={SERVICES}
+            onChange={onChangeLeadTimes}/>
+          <SettingsComp
+            SERVICES={SERVICES}
+            settings={SETTINGS}
+            onChangeTimeStep={onChangeTimeStep}
+            onChangeAdditionalPizzaLeadTime={onChangeAdditionalPizzaLeadTime}
+            onAddOperatingHours={addOperatingHours}
+            onRemoveOperatingHours={removeOperatingHours}
+          />
+          <button className="btn btn-dark" onClick={onSubmit} disabled={!canSubmit}>Update Settings</button>
+        </div>
 
 
-    );
-  }
+  );
 }
 
 export default App;
