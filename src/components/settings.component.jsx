efb909ff4404905552764cdf9f2ff4f2e1929f54
Dave Lichterman <dave@windycitypie.com>
2020-03-04 22:32:08 -0800

    convert settings component to newfangled component

--- src/components/settings.component.jsx
+++ src/components/settings.component.jsx
@@ -1,4 +1,4 @@
-import React, { useState, Component } from 'react';
+import React, { useState } from 'react';
 import Moment from 'react-moment';
 import memoizeOne from 'memoize-one';
 
@@ -58,115 +58,119 @@ const OperatingHoursIntervalForm = ({
   )
 }
 
-export default class SettingsComponent extends Component {
-  constructor(props) {
-    super(props);
-    this.state = {
-      operating_hours_form_intervals: Array(this.props.SERVICES.length).fill(),
-    };
+const SettingsComponent = ({
+  onAddOperatingHours,
+  onChangeAdditionalPizzaLeadTime,
+  onRemoveOperatingHours,
+  onChangeTimeStep,
+  SERVICES,
+  settings
+}) => {
 
-    //TODO: this is derived state, so this won't work if SERVICES changes
-    for (var i in this.state.operating_hours_form_intervals) {
-      this.state.operating_hours_form_intervals[i] = Array(7).fill({start: null, end:null});
+  const GenerateInitialOperatingHoursFormIntervals = (num_services) => {
+    const intervals = Array(num_services).fill();
+    for (var i in intervals) {
+      intervals[i] = Array(7).fill({start: null, end:null});
     }
+    return intervals;
   }
-  onSetUpperBound = (service_index, day_index, e) => {
-    const new_intervals = JSON.parse(JSON.stringify(this.state.operating_hours_form_intervals));
+  const [operating_hours_form_intervals, setOperatingHoursFormIntervals] = useState(GenerateInitialOperatingHoursFormIntervals(SERVICES.length));
+  
+  const onSetUpperBound = (service_index, day_index, e) => {
+    const new_intervals = JSON.parse(JSON.stringify(operating_hours_form_intervals));
     new_intervals[service_index][day_index] = {start: new_intervals[service_index][day_index].start, end: e};
-    this.setState({operating_hours_form_intervals: new_intervals});
+    setOperatingHoursFormIntervals(new_intervals);
   }
-  onSetLowerBound = (service_index, day_index, e) => {
-    const new_intervals = JSON.parse(JSON.stringify(this.state.operating_hours_form_intervals));
+  const onSetLowerBound = (service_index, day_index, e) => {
+    const new_intervals = JSON.parse(JSON.stringify(operating_hours_form_intervals));
     let new_end = new_intervals[service_index][day_index].end;
     if (e && new_end && new_end.value < e.value) {
       new_end = null;
     }
     new_intervals[service_index][day_index] = {start: e, end: new_end};
-    this.setState({operating_hours_form_intervals: new_intervals});
+    setOperatingHoursFormIntervals(new_intervals);
   }
-  AddOperatingHoursInterval = (service_index, day_index) => {
-    const interval = this.state.operating_hours_form_intervals[service_index][day_index];
-    this.props.onAddOperatingHours(
+  const AddOperatingHoursInterval = (service_index, day_index) => {
+    const interval = operating_hours_form_intervals[service_index][day_index];
+    onAddOperatingHours(
       service_index,
       day_index,
       [interval.start.value, interval.end.value]);
-    this.onSetLowerBound(service_index, day_index, null);
-    this.onSetUpperBound(service_index, day_index, null);
-    const new_intervals = JSON.parse(JSON.stringify(this.state.operating_hours_form_intervals));
+    onSetLowerBound(service_index, day_index, null);
+    onSetUpperBound(service_index, day_index, null);
+    const new_intervals = JSON.parse(JSON.stringify(operating_hours_form_intervals));
     new_intervals[service_index][day_index] = {start: null, end: null};
-    this.setState({operating_hours_form_intervals: new_intervals});
+    setOperatingHoursFormIntervals(new_intervals);
     console.log(new_intervals[service_index][day_index]);
   }
 
 
-  render() {
-    const operating_hours_service_html = this.props.settings.operating_hours.map((operating_hours_week, h) => {
-      const operating_hours_week_html = operating_hours_week.map((operating_hours_day, i) => {
-        const operating_hours_day_intervals_html = operating_hours_day.map((interval, j) => {
-          return (
-            <div className="col-1.5 mr-5" key={j}>
-              {WDateUtils.MinutesToPrintTime(interval[0])}
-              &nbsp;-&nbsp;
-              {WDateUtils.MinutesToPrintTime(interval[1])}
-              <button
-                className="btn btn-light"
-                type="button"
-                onClick={() => this.props.onRemoveOperatingHours(h, i, j)}>x
-              </button>
-            </div>
-          );
-        });
+  const operating_hours_service_html = settings.operating_hours.map((operating_hours_week, h) => {
+    const operating_hours_week_html = operating_hours_week.map((operating_hours_day, i) => {
+      const operating_hours_day_intervals_html = operating_hours_day.map((interval, j) => {
         return (
-          <div className="row" key={i}>
-            <div className="col-1 ml-3">
-              <Moment format="dddd" parse="e">{i}</Moment>:
-            </div>
-            {operating_hours_day_intervals_html}
-            <div className="col-7 ml-md-auto">
-            <OperatingHoursIntervalForm
-              disabled={false}
-              interval={this.state.operating_hours_form_intervals[h][i]}
-              onChangeLowerBound={e => this.onSetLowerBound(h, i, e)}
-              onChangeUpperBound={e => this.onSetUpperBound(h, i, e)}
-              onAddOperatingHours={e => this.AddOperatingHoursInterval(h, i)}
-              settings={this.props.settings}
-              />
-              </div>
+          <div className="col-1.5 mr-5" key={j}>
+            {WDateUtils.MinutesToPrintTime(interval[0])}
+            &nbsp;-&nbsp;
+            {WDateUtils.MinutesToPrintTime(interval[1])}
+            <button
+              className="btn btn-light"
+              type="button"
+              onClick={() => onRemoveOperatingHours(h, i, j)}>x
+            </button>
           </div>
         );
       });
       return (
-        <span key={h}>
-          <h5>{this.props.SERVICES[h]}:</h5>
-          {operating_hours_week_html}
-        </span>
+        <div className="row" key={i}>
+          <div className="col-1 ml-3">
+            <Moment format="dddd" parse="e">{i}</Moment>:
+          </div>
+          {operating_hours_day_intervals_html}
+          <div className="col-7 ml-md-auto">
+          <OperatingHoursIntervalForm
+            disabled={false}
+            interval={operating_hours_form_intervals[h][i]}
+            onChangeLowerBound={e => onSetLowerBound(h, i, e)}
+            onChangeUpperBound={e => onSetUpperBound(h, i, e)}
+            onAddOperatingHours={e => AddOperatingHoursInterval(h, i)}
+            settings={settings}
+            />
+            </div>
+        </div>
       );
     });
     return (
-      <span>
-        <div className="row no-gutters form-inline">
-          <label>Additional lead time per pizza beyond the first: </label>
-          <CheckedInputComponent
-            type="number"
-            className="form-control"
-            //InputCheckFunction={CheckedInput.CheckForNumberGTZero}
-            value={this.props.settings.additional_pizza_lead_time}
-            onFinishChanging={this.props.onChangeAdditionalPizzaLeadTime}
-          />
-        </div>
-        <div className="row no-gutters form-inline">
-          <label>Time Step: </label>
-          <CheckedInputComponent
-            type="number"
-            className="form-control"
-            //InputCheckFunction={CheckedInput.CheckForNumberGTZero}
-            value={this.props.settings.time_step}
-            onFinishChanging={this.props.onChangeTimeStep}
-          />
-        </div>
-        {operating_hours_service_html}
+      <span key={h}>
+        <h5>{SERVICES[h]}:</h5>
+        {operating_hours_week_html}
       </span>
-    )
-  }
-
+    );
+  });
+  return (
+    <span>
+      <div className="row no-gutters form-inline">
+        <label>Additional lead time per pizza beyond the first: </label>
+        <CheckedInputComponent
+          type="number"
+          className="form-control"
+          value={settings.additional_pizza_lead_time}
+          onFinishChanging={onChangeAdditionalPizzaLeadTime}
+        />
+      </div>
+      <div className="row no-gutters form-inline">
+        <label>Time Step: </label>
+        <CheckedInputComponent
+          type="number"
+          className="form-control"
+          value={settings.time_step}
+          onFinishChanging={onChangeTimeStep}
+        />
+      </div>
+      {operating_hours_service_html}
+    </span>
+  )
 }
+
+
+export default SettingsComponent;
