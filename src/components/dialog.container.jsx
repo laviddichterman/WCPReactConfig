76c90cae489026190faf724b7374e9a676e01d24
Dave Lichterman <dave@windycitypie.com>
2020-06-20 13:17:46 -0700

    pass through props to dialog container

--- src/components/dialog.container.jsx
+++ src/components/dialog.container.jsx
@@ -3,9 +3,9 @@ import DialogTitle from '@material-ui/core/DialogTitle';
 import Dialog from '@material-ui/core/Dialog';
 import Divider from '@material-ui/core/Divider';
 
-function DialogContainer({ onClose, title, inner_component, isOpen }) {
+function DialogContainer({ onClose, title, inner_component, isOpen, ...other }) {
   return (
-    <Dialog onClose={onClose} aria-labelledby="simple-dialog-title" open={isOpen}>
+    <Dialog {...other} onClose={onClose} aria-labelledby="simple-dialog-title" open={isOpen}>
       <DialogTitle id="simple-dialog-title">{title}</DialogTitle>
       <Divider />
       {inner_component}
