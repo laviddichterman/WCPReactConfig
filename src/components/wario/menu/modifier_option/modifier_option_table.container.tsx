af587c7f95dfd4ee666c10fa4561958c5e2db2bb
Dave Lichterman <me@lavid.me>
2024-08-16 18:39:02 -0700

    v 0.4.64 fix modifier option table container fields

--- src/components/wario/menu/modifier_option/modifier_option_table.container.tsx
+++ src/components/wario/menu/modifier_option/modifier_option_table.container.tsx
@@ -2,7 +2,7 @@ import { format } from 'date-fns'
 import { GridActionsCellItem, GridRowParams } from "@mui/x-data-grid-premium";
 import { Edit, DeleteOutline, BedtimeOff, CheckCircle, Cancel } from "@mui/icons-material";
 import Tooltip from '@mui/material/Tooltip';
-import { DisableDataCheck, DISABLE_REASON, IOption, IOptionType } from '@wcp/wcpshared';
+import { DisableDataCheck, DISABLE_REASON, IMoney, IOption, IOptionType, IWInterval } from '@wcp/wcpshared';
 import TableWrapperComponent from "../../table_wrapper.component";
 import { useAppDispatch, useAppSelector } from "../../../../hooks/useRedux";
 import { openModifierOptionDelete, openModifierOptionDisable, openModifierOptionDisableUntilEod, openModifierOptionEdit, openModifierOptionEnable } from "../../../../redux/slices/CatalogSlice";
@@ -65,18 +65,18 @@ const ModifierOptionTableContainer = ({
               return DisableDataCheck(params.row.disabled, [], CURRENT_TIME).enable !== DISABLE_REASON.ENABLED ? [EDIT_MODIFIER_OPTION, ENABLE_MODIFIER_OPTION, DELETE_MODIFIER_OPTION] : [EDIT_MODIFIER_OPTION, DISABLE_MODIFIER_OPTION_UNTIL_EOD, DISABLE_MODIFIER_OPTION, DELETE_MODIFIER_OPTION];
             }
           },
-          { headerName: "Name", field: "item.display_name", valueGetter: (v: { row: IOption }) => v.row.displayName, flex: 1 },
-          { headerName: "Price", field: "item.price.amount", valueGetter: (v: { row: IOption }) => `$${Number(v.row.price.amount / 100).toFixed(2)}` },
-          { headerName: "Shortcode", field: "item.shortcode", valueGetter: (v: { row: IOption }) => v.row.shortcode, },
-          { headerName: "Description", field: "item.description", valueGetter: (v: { row: IOption }) => v.row.description, },
-          { headerName: "Ordinal", field: "ordinal", valueGetter: (v: { row: IOption }) => v.row.ordinal },
-          { headerName: "FFactor", field: "metadata.flavor_factor", valueGetter: (v: { row: IOption }) => v.row.metadata.flavor_factor },
-          { headerName: "BFactor", field: "metadata.bake_factor", valueGetter: (v: { row: IOption }) => v.row.metadata.bake_factor },
-          { headerName: "Can Split?", field: "metadata.can_split", valueGetter: (v: { row: IOption }) => v.row.metadata.can_split },
-          { headerName: "EnableFxn", field: "enable_function.name", valueGetter: (v: { row: IOption }) => v.row.enable ? productInstanceFunctions[v.row.enable].name : "" },
+          { headerName: "Name", field: "displayName", flex: 1 },
+          { headerName: "Price", field: "price", valueGetter: (v: IMoney) => `$${Number(v.amount / 100).toFixed(2)}` },
+          { headerName: "Shortcode", field: "shortcode" },
+          { headerName: "Description", field: "description" },
+          { headerName: "Ordinal", field: "ordinal" },
+          { headerName: "FFactor", field: "metadata.flavor_factor" },
+          { headerName: "BFactor", field: "metadata.bake_factor" },
+          { headerName: "Can Split?", field: "metadata.can_split" },
+          { headerName: "EnableFxn", field: "enable", valueGetter: (v: string) => v ? productInstanceFunctions[v].name : "" },
           // we pass null instead of actually passing the availability because we want to make a decision based on just the .disabled value
           // eslint-disable-next-line no-nested-ternary 
-          { headerName: "Disabled", field: "item.disabled", valueGetter: (v: { row: IOption }) => (v.row.disabled !== null && DisableDataCheck(v.row.disabled, [], CURRENT_TIME).enable !== DISABLE_REASON.ENABLED ? (v.row.disabled.start > v.row.disabled.end ? "True" : `${format(v.row.disabled.start, "MMMM dd, y hh:mm a")} to ${format(v.row.disabled.end, "MMMM dd, y hh:mm a")}`) : "False") },
+          { headerName: "Disabled", field: "disabled", valueGetter: (v: IWInterval | null) => (v !== null && DisableDataCheck(v, [], CURRENT_TIME).enable !== DISABLE_REASON.ENABLED ? (v.start > v.end ? "True" : `${format(v.start, "MMMM dd, y hh:mm a")} to ${format(v.end, "MMMM dd, y hh:mm a")}`) : "False") },
         ]}
         getRowId={(row) => row._id}
         rows={modifier_types_map[modifierType.id].options.map(x => modifierOptionsMap[x]).sort((a,b)=>a.ordinal - b.ordinal)}
